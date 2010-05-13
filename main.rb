bc4d9739903f72df1a4d82bc4f0e7b9a6f6c7dfe
Brad Johansen <johansenbradley@gmail.com>
2010-05-13 19:58:23 -0400

    Added configurations and more routes

--- main.rb
+++ main.rb
@@ -1,6 +1,32 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
 
+# Set configurations
+set :public, File.dirname(__FILE__) + '/static'
+set :views, File.dirname(__FILE__) + '/views'
+set :haml, {:format => :html5}
+
+# Sass setup
+get '/css/screen.css' do
+	content-type 'text/css', :charset => 'utf-8'
+	sass :screen
+end
+
+get '/css/print.css' do
+	content-type 'text/css', :charset => 'utf-8'
+	sass :print
+end
+
+# Routes
 get '/' do 
-	'Hello World'
+	haml :index
+end
+
+get '/hospitals'do
+	haml :hospitals
+end
+
+get '/about' do
+	haml :about 
 end
