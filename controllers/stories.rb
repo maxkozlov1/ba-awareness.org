389f6c979fd21ab0a88fecd5c85497ca541982ed
Brad Johansen <johansenbradley@gmail.com>
2010-06-10 12:35:22 -0400

    Added the Symptoms page and re-linked everything

--- controllers/stories.rb
+++ controllers/stories.rb
@@ -1,3 +1,7 @@
+get '/stories' do
+  erb :"stories/index"
+end
+
 get '/stories/ashley-johansen' do
   erb :"stories/ashley"
 end
--- controllers/whatisit.rb
+++ controllers/whatisit.rb
@@ -1,3 +1,7 @@
 get '/whatisit/whatisba' do
   erb :"/whatisit/whatisba"
 end
+
+get '/whatisit/symptoms' do
+    erb :"/whatisit/symptoms"
+end
